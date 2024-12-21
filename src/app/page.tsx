5d0c1ff0f0e02ec853953409875adb6ef0d08d91
SAMUEL <sleonardot@outlook.com>
2024-12-20 20:47:02 -0500

    "Fixing errors related with 'npm run build' "

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Skills from "./Components/Skills";
 import Education from "./Components/Education";
 import Main from "./Components/Main";
 import Nav from "./Components/Nav";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
   const [darkMode, setDarkMode] = React.useState(false);
