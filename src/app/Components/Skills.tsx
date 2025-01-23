05baf2984222adcdb105a9a606756cca29e1d155
SAMUEL <sleonardot@outlook.com>
2025-01-23 18:48:57 -0500

    Guardando los datos de la sección 'Skills' en un js

--- src/app/Components/Skills.tsx
+++ src/app/Components/Skills.tsx
@@ -1,162 +1,42 @@
 import React from "react";
 import { FullCircle } from "./icons/FullCircle";
-import css from "../img/programming-icons/css.svg";
-import html from "../img/programming-icons/html.svg";
-import javascript from "../img/programming-icons/javascript.svg";
-import python from "../img/programming-icons/python.svg";
-import react from "../img/programming-icons/react.svg";
-import tailwindcss from "../img/programming-icons/tailwindcss.svg";
 import Image from "next/image";
+import css from '../img/programming-icons/css.svg';
+import { skills } from "../data/Skills"; 
 
-const Skills = () => {
+function Skills() {
   return (
     <section
       id="skills"
       className="  w-full mt-20   flex flex-col py-6 my-20 px-6 sm:px-8 lg:px-32"
     >
-      <div>
+
         <h2 className="text-shadow font-montserrat text-neutral-900 dark:text-neutral-300  text-center mb-10 font-bold text-4xl sm:text-2xl md:text-3xl lg:text-4xl">
           SKILLS
         </h2>
         <div className="grid mt-16 auto-cols-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4   ">
-          <div className="flex flex-col items-start bg-slate-800 hover:scale-105 shadow-md  cursor-pointer   hover:brightness-110 transition rounded-lg p-6 border-blue-500 shadow-blue-500">
-            <Image
-              width={200}
-              height={200}
-              className="w-32 place-self-center "
-              src={html.src}
-              alt="html"
-            />
-            <h1 className="text-white font-rubik   font-semibold sm:text-md md:text-xl lg:text-2xl">
-              HTML5{" "}
-            </h1>
-            <h4 className="text-white   font-rubik text-lg mb-1 ">
-              Level: Intermediate{" "}
-            </h4>
-            <div className="flex flex-row gap-1">
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-gray-400" />
+          {skills.map((skill, index) => (
+            <div
+            key={index}
+            className="flex flex-col items-start bg-slate-800 hover:scale-105 shadow-md cursor-pointer hover:brightness-110 transition roundend-lg p-6 border-blue-500 shadow-blue-500">
+              <img
+              width={400}
+              height={400}
+              className="w-32 place-self-center"
+              src={skill.image}
+              alt={skill.name}
+              />
+              <h1 className="text-white font-rubik font-semibold sm:text-lg md:text-xl lg:text-2xl">{skill.name}</h1>
+              <h4 className="text-white font-rubik text-lg mb-1">
+                Level: {skill.level}
+              </h4>
+              <div className="flex flex-row gap-1">
+                {[...Array(5)].map((_, i) => (
+                  <FullCircle key={i} className={i < skill.stars ? "text-blue-300" : "text-gray-400"} />
+                ))}
+              </div>
             </div>
-          </div>
-
-          <div className="flex flex-col items-start bg-slate-800 hover:scale-105 shadow-md  cursor-pointer  hover:brightness-110 transition   rounded-lg p-6 border-blue-500 shadow-blue-500">
-            <Image
-              className="w-32 place-self-center "
-              src={css.src}
-              alt="html"
-              width={200}
-              height={200}
-            />
-            <h1 className="text-white font-rubik font-semibold sm:text-lg md:text-xl lg:text-2xl">
-              CSS3{" "}
-            </h1>
-            <h4 className="text-white  font-rubik text-lg mb-1 ">
-              Level: Intermediate{" "}
-            </h4>
-            <div className="flex flex-row gap-1">
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-gray-400" />
-            </div>
-          </div>
-
-          <div className="flex flex-col items-start bg-slate-800 hover:scale-105 shadow-md  cursor-pointer  hover:brightness-110 transition   rounded-lg p-6 border-blue-500 shadow-blue-500">
-            <Image
-              className="w-32 place-self-center mb-2 "
-              width={200}
-              height={200}
-              src={javascript.src}
-              alt="html"
-            />
-            <h1 className="text-white  font-rubik font-semibold sm:text-lg md:text-xl lg:text-2xl">
-              Javascript{" "}
-            </h1>
-            <h4 className="text-white  font-rubik text-lg mb-1 ">
-              Level: Beginner{" "}
-            </h4>
-            <div className="flex flex-row gap-1">
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-gray-400" />
-              <FullCircle className="text-gray-400" />
-            </div>
-          </div>
-
-          <div className="flex flex-col items-start bg-slate-800 hover:scale-105 shadow-md cursor-pointer  hover:brightness-110 transition   rounded-lg p-6 border-blue-500 shadow-blue-500">
-            <Image
-            width={200}
-            height={200}
-              className="w-32 place-self-center "
-              src={react.src}
-              alt="html"
-            />
-            <h1 className="text-white   font-rubik font-semibold sm:text-lg md:text-xl lg:text-2xl">
-              React{" "}
-            </h1>
-            <h4 className="text-white  font-rubik text-lg mb-1 ">
-              Level: Intermediate{" "}
-            </h4>
-            <div className="flex flex-row gap-1">
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-gray-400" />
-              <FullCircle className="text-gray-400" />
-            </div>
-          </div>
-
-          <div className="flex flex-col items-start bg-slate-800 hover:scale-105 shadow-md  cursor-pointer  hover:brightness-110 transition   rounded-lg p-6 border-blue-500 shadow-blue-500">
-            <Image
-            width={200}
-            height={200}
-              className="w-32 place-self-center "
-              src={tailwindcss.src}
-              alt="html"
-            />
-            <h1 className="text-white  font-rubik font-semibold sm:text-lg md:text-xl lg:text-2xl">
-              Tailwind CSS{" "}
-            </h1>
-            <h4 className="text-white  font-rubik text-lg mb-1 ">
-              Level: Intermediate{" "}
-            </h4>
-            <div className="flex flex-row gap-1">
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-gray-400" />
-            </div>
-          </div>
-
-          <div className="flex flex-col items-start bg-slate-800 hover:scale-105 shadow-md  cursor-pointer  hover:brightness-110 transition   rounded-lg p-6 border-blue-500 shadow-blue-500">
-            <Image
-            width={200}
-            height={200}
-              className="w-32 place-self-center "
-              src={python.src}
-              alt="html"
-            />
-            <h1 className="text-white font-rubik font-semibold sm:text-lg md:text-xl lg:text-2xl">
-              Python{" "}
-            </h1>
-            <h4 className="text-white  font-rubik text-lg mb-1 ">
-              Level: Beginner{" "}
-            </h4>
-            <div className="flex flex-row gap-1">
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-blue-300" />
-              <FullCircle className="text-gray-400" />
-              <FullCircle className="text-gray-400" />
-              <FullCircle className="text-gray-400" />
-            </div>
-          </div>
-        </div>
+          ))}
       </div>
     </section>
   );
---
+++ src/app/data/Skills.js
@@ -0,0 +1,50 @@
+
+export const skills = [ 
+    {
+        id: 1,
+        name: "HTML5",
+        image: "/programming-icons/html.svg",
+        level: "Intermediate", 
+        stars: 4, 
+    },
+
+    {
+        id: 2,
+        name: "CSS3",
+        image: "/programming-icons/css.svg",
+        level: "Intermediate", 
+        stars: 4, 
+    }, 
+
+    {
+        id: 3,
+        name: "JavaScript",
+        image: "/programming-icons/javascript.svg",
+        level: "Intermediate", 
+        stars: 4,  
+    },
+
+    {
+        id: 4,
+        name: "React",
+        image: "/programming-icons/react.svg",
+        level: "Intermediate", 
+        stars: 3, 
+    },
+
+    {
+        id: 5,
+        name: "TailwindCSS",
+        image: "/programming-icons/tailwindcss.svg",
+        level: "Intermediate", 
+        stars: 4, 
+    },
+
+    {
+        id: 6,
+        name: "Python",
+        image: "/programming-icons/python.svg",
+        level: "Intermediate", 
+        stars: 3, 
+    }
+]
